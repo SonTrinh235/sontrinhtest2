74243347ecc334efddcbd791419dfd67748c4b2d
sontrinh235 <ducsondz2005@gmail.com>
2025-02-17 20:58:46 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ function handleRightClick() {
     window.location.href = "Right.html";
 }
 
+let idx =1;
+
 function handleLeftClick() {
     window.location.href = "Wrong.html";
+}
+
+function handleReturn(){
+    window.location.href = "index.html";
 }
